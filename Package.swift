c78b24b270ad6ad45f90407db5e23a96ca9e8651
Benjamin Truitt <ben.truitt@gmail.com>
2025-01-07 00:27:09 -0600

    Rename library to avoid conflict with MacPaw.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-	name: "OpenAI",
+	name: "SwiftRealtimeOpenAI",
 	platforms: [
 		.iOS(.v17),
 		.tvOS(.v17),
@@ -13,7 +13,7 @@ let package = Package(
 		.macCatalyst(.v17),
 	],
 	products: [
-		.library(name: "OpenAI", type: .static, targets: ["OpenAI"]),
+		.library(name: "SwiftRealtimeOpenAI", type: .static, targets: ["SwiftRealtimeOpenAI"]),
 	],
 	dependencies: [
 		.package(url: "https://github.com/stasel/WebRTC.git", branch: "latest"),
